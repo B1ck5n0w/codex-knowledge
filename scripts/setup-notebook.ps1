50d3378a4e71bc81be386f13991a85e0c731995a
B1ck5n0w <kontakt@chris-derix.de>
2026-07-20 08:06:18 +0200

    Process Planetarium zur Synchronisation hinzufügen

--- scripts/setup-notebook.ps1
+++ scripts/setup-notebook.ps1
@@ -40,6 +40,7 @@ Initialize-GitHubCredentials
 Get-OrCloneProject -Repository 'https://github.com/B1ck5n0w/sommer-party.git' -Target (Join-Path $DocumentsRoot 'Sommer Party')
 Get-OrCloneProject -Repository 'https://github.com/B1ck5n0w/freizeitexperten-erp-dev.git' -Target (Join-Path $DocumentsRoot 'freizeitexperten.de\freizeitexperten-erp-dev-git')
 Get-OrCloneProject -Repository 'https://github.com/B1ck5n0w/freizeitexperten-plugins-work.git' -Target (Join-Path $DocumentsRoot 'freizeitexperten.de\plugins-work')
+Get-OrCloneProject -Repository 'https://github.com/B1ck5n0w/process-planetarium.git' -Target (Join-Path $DocumentsRoot 'Process Planetarium')
 
 $driveRelativePath = 'Meine Ablage\Projekte\KI Projekte'
 $driveProjectRoot = Get-PSDrive -PSProvider FileSystem |
